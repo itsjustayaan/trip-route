e8695f8e0bb80369453213ca5dabb42a73ece5cc
Ayaan <ayaan2536@gmail.com>
2023-06-17 17:05:54 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function initMap() {
+  let location = { lat: 26.924150519735488, lng: 80.95402479171754 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: location,
+    
+  });
+}
+window.initMap = initMap;
