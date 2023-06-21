80c86e213c8cc64bf67f54400107352c6ddd4666
Ayaan <ayaan2536@gmail.com>
2023-06-21 18:45:40 +0530

    Lat Lng are readable and markers are placed on the map

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+let map;
+let markers = [];
 function initMap() {
   let location = { lat: 26.924150519735488, lng: 80.95402479171754 };
-  const map = new google.maps.Map(document.getElementById("map"), {
+  map = new google.maps.Map(document.getElementById("map"), {
     zoom: 4,
     center: location,
   });
@@ -8,11 +10,61 @@ function initMap() {
 window.initMap = initMap;
 $(document).ready(function () {
   $("#makeRoute").on("click", function () {
+
+    let addresses = [];
+    let selectedLocation = [];
+    
     let start = $("#start").val();
+    addresses.push(start);
     let end = $("#end").val();
-    let stops = [];
     $(".inputTag").each(function () {
-      stops.push($(this).children("input").val());
+      addresses.push($(this).children("input").val());
+    });
+    addresses.push(end);
+    
+    addresses.forEach((address) => {
+      const encodedAddress = encodeURIComponent(address);
+      const apiUrl = `https://nominatim.openstreetmap.org/search?format=json&q=${encodedAddress}`;
+
+      fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.length > 0) {
+            const result = data[0];
+
+            let latLang = { lat: parseFloat(result.lat), lng: parseFloat(result.lon) };
+
+            let marker = new google.maps.Marker({
+              position: latLang,
+              map: map,
+              draggable: true,
+            });
+
+            marker.addListener("dragend", (markerDragEvent) => {
+              let index = markers.indexOf(marker);
+              selectedLocation[index] = markerDragEvent.latLng.toJSON();
+            });
+
+            marker.addListener("dblclick", () => {
+              let index = markers.indexOf(marker);
+              markers.splice(index, 1);
+              selectedLocation.splice(index, 1);
+              marker.setMap(null);
+            });
+            
+            selectedLocation.push(latLang);
+            markers.push(marker);
+
+          } else {
+            console.log(`No results found for the address: ${address}`);
+          }
+        })
+        .catch((error) => {
+          console.error(
+            `Error occurred while geocoding address: ${address}`,
+            error
+          );
+        });
     });
   });
 });
