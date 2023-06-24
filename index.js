e29a46f074e48bf9a7e3e7d25cdfa056c0708f50
Ayaan <ayaan2536@gmail.com>
2023-06-24 21:46:32 +0530

    Updates Done

--- index.js
+++ index.js
@@ -1,70 +1,141 @@
 let map;
 let markers = [];
+let Paths = [];
+let selectedLocation = [];
+let isPresent = false;
+
 function initMap() {
   let location = { lat: 26.924150519735488, lng: 80.95402479171754 };
   map = new google.maps.Map(document.getElementById("map"), {
     zoom: 4,
     center: location,
   });
+  map.addListener("click", async (mapsMouseEvent) => {
+    let marker = new google.maps.Marker({
+      position: mapsMouseEvent.latLng,
+      map: map,
+      draggable: true,
+    });
+    const url = `https://nominatim.openstreetmap.org/reverse?lat=${marker.position.lat()}&lon=${marker.position.lng()}&format=json`;
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      const address = data.display_name;
+      assignAddress(address);
+    } catch (error) {
+      console.error(error);
+    }
+    markers.push(marker);
+
+    // marker.addListener("dragend", (markerDragEvent) => {
+    //   let index = markers.indexOf(marker);
+    //   console.log(index);
+    // });
+  });
 }
 window.initMap = initMap;
+
 $(document).ready(function () {
-  $("#makeRoute").on("click", function () {
+  $("#makeRoute").on("click", async function () {
+    let addresses = {
+      address: [],
+      latLang: [],
+      plotPoints: [],
+    };
+    if (Paths.length > 0) Paths.pop().setMap(null);
 
-    let addresses = [];
-    let selectedLocation = [];
-    
+    markers.forEach((marker) => {
+      marker.setMap(null);
+    });
+    markers = [];
+
     let start = $("#start").val();
-    addresses.push(start);
-    let end = $("#end").val();
+    addresses.address.push(start);
+
     $(".inputTag").each(function () {
-      addresses.push($(this).children("input").val());
+      addresses.address.push($(this).children("input").val());
     });
-    addresses.push(end);
-    
-    addresses.forEach((address) => {
-      const encodedAddress = encodeURIComponent(address);
-      const apiUrl = `https://nominatim.openstreetmap.org/search?format=json&q=${encodedAddress}`;
 
-      fetch(apiUrl)
-        .then((response) => response.json())
-        .then((data) => {
-          if (data.length > 0) {
-            const result = data[0];
+    let end = $("#end").val();
+    addresses.address.push(end);
 
-            let latLang = { lat: parseFloat(result.lat), lng: parseFloat(result.lon) };
+    for (const address of addresses.address) {
+      try {
+        const encodedAddress = encodeURIComponent(address);
+        const apiUrl = `https://nominatim.openstreetmap.org/search?format=json&q=${encodedAddress}`;
+        const response = await fetch(apiUrl);
+        const data = await response.json();
 
-            let marker = new google.maps.Marker({
-              position: latLang,
-              map: map,
-              draggable: true,
-            });
+        if (data.length > 0) {
+          const result = data[0];
 
-            marker.addListener("dragend", (markerDragEvent) => {
-              let index = markers.indexOf(marker);
-              selectedLocation[index] = markerDragEvent.latLng.toJSON();
-            });
+          let latLang = {
+            lng: parseFloat(result.lon),
+            lat: parseFloat(result.lat),
+          };
 
-            marker.addListener("dblclick", () => {
-              let index = markers.indexOf(marker);
-              markers.splice(index, 1);
-              selectedLocation.splice(index, 1);
-              marker.setMap(null);
-            });
-            
-            selectedLocation.push(latLang);
-            markers.push(marker);
+          let marker = new google.maps.Marker({
+            position: latLang,
+            map: map,
+            draggable: true,
+          });
 
-          } else {
-            console.log(`No results found for the address: ${address}`);
-          }
-        })
-        .catch((error) => {
-          console.error(
-            `Error occurred while geocoding address: ${address}`,
-            error
-          );
-        });
+          addresses.latLang.push(latLang);
+          addresses.plotPoints.push([latLang.lng, latLang.lat]);
+          markers.push(marker);
+
+          // marker.addListener("dragend", (markerDragEvent) => {
+          //   let index = markers.indexOf(marker);
+          //   console.log(index);
+          // });
+        } else {
+          console.log(`No results found for the address: ${address}`);
+        }
+      } catch (error) {
+        console.error(
+          `Error occurred while geocoding address: ${address}`,
+          error
+        );
+      }
+    }
+    const data = await drawRoute(addresses.plotPoints);
+    let plotlines = [];
+    data.paths[0].points.coordinates.forEach((coordinate) => {
+      plotlines.push({ lat: coordinate[1], lng: coordinate[0] });
     });
+    const makePath = new google.maps.Polyline({
+      path: plotlines,
+      geodesic: true,
+      strokeColor: "#E57C23",
+      strokeOpacity: 1.0,
+      strokeWeight: 2,
+    });
+    makePath.setMap(map);
+    Paths.push(makePath);
   });
 });
+
+async function drawRoute(latLang) {
+  const query = new URLSearchParams({
+    key: "39bf67bf-9dcb-4a43-8c43-e14a86224002",
+  }).toString();
+
+  const resp = await fetch(`https://graphhopper.com/api/1/route?${query}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      points: latLang,
+      vehicle: "bike",
+      locale: "en",
+      instructions: true,
+      calc_points: true,
+      points_encoded: false,
+    }),
+  });
+
+  const data = await resp.json();
+  return data;
+}
--- jQscript.js
+++ jQscript.js
@@ -1,13 +1,31 @@
+let n = 1;
 $("document").ready(function () {
-    let n=1;
-    $("#addStops").on("click", function () {
-      var inputId = "inputdiv_" + n; 
-      var input = $('<div class="inputTag" id="'+inputId+'"><input type="text" placeholder="In Between"><span class="material-icons remove-icon">remove</span></div>');
-      $("#innerContainer").append(input);
-      n++;
-    });
-    $("#innerContainer").on("click", ".remove-icon", function() {
-        var inputTagId = $(this).parent(".inputTag").attr("id");
-        $("#" + inputTagId).remove();
-      });
+  $("#addStops").on("click", function () {
+    var inputId = "inputdiv_" + n;
+    var input = $(
+      '<div class="inputTag" id="' +
+        inputId +
+        '"><input type="text" placeholder="In Between"><span class="material-icons remove-icon">remove</span></div>'
+    );
+    $("#innerContainer").append(input);
+    n++;
   });
+  $("#innerContainer").on("click", ".remove-icon", function () {
+    var inputTagId = $(this).parent(".inputTag").attr("id");
+    var index = inputTagId.indexOf('_');
+    var inputNumber = inputTagId.substring(index + 1);
+    $("#" + inputTagId).remove();
+  });
+});
+function assignAddress(address) {
+  var inputId = "inputdiv_" + n;
+  var input = $(
+    '<div class="inputTag" id="' +
+      inputId +
+      '"><input type="text" placeholder="In Between" value="' +
+      address +
+      '"><span class="material-icons remove-icon">remove</span></div>'
+  );
+  $("#innerContainer").append(input);
+  n++;
+}
